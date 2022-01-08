2591d64a8aa004ea5f5e45f8b11bbe2ea58574bb
Arnab Jana <97357557+ferdinand-ypsilant@users.noreply.github.com>
2022-01-08 23:50:06 +0530

    the code is here...

---
+++ Code to Design-of-names.py
@@ -0,0 +1,108 @@
+
+glostrixin_hydrotermius = input("Name: \n\n")
+print("_____________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________Glostrixin Hydrotermius___________________________________________________________________________________________________________________________________________________________________________________________________________________________________________")
+for c in glostrixin_hydrotermius:
+
+
+    c = c.upper()
+
+    if (c == "A"):
+
+        print("  ######  \n  #    #  \n  ######  \n  #    #  \n  #    #  \n\n")
+
+
+    elif (c == "B"):  #Glostrixin Hydrotermius
+
+        print("  ######  \n  #    #  \n  #####   \n  #    #  \n  ######  \n\n")
+
+
+    elif (c == "C"):
+
+        print("  ######  \n  #       \n  #       \n  #       \n  ######  \n\n")
+#Glostrixin Hydrotermius
+
+    elif (c == "D"):
+
+        print("  #####   \n  #    #  \n  #    #  \n  #    #  \n  #####   \n\n")
+
+
+    elif (c == "E"):
+
+        print("  ######  \n  #       \n  #####   \n  #       \n  ######  \n\n")
+
+
+    elif (c == "F"):
+
+        print("  ######  \n  #       \n  #####   \n  #       \n  #       \n\n")#Glostrixin Hydrotermius
+
+
+    elif (c == "G"):
+        print("   #####  \n  #       \n  #  ##   \n  #   ##  \n   ### #  \n\n")
+    
+    elif (c == "H"):
+        print("  #    #  \n  #    #  \n  ######  \n  #    #  \n  #    #  \n\n")
+    
+    elif (c == "I"):#Glostrixin Hydrotermius
+        print("  ######  \n    ##    \n    ##    \n    ##    \n  ######  \n\n")
+    
+    elif (c == "J"):
+        print("  ######  \n    ##    \n    ##    \n  # ##    \n  ####    \n\n")
+    
+    elif (c == "K"):
+        print("  #   #   \n  #  #    \n  ##      \n  #  #    \n  #   #   \n\n")
+    #Glostrixin Hydrotermius
+    elif (c == "L"):
+        print("  #       \n  #       \n  #       \n  #       \n  ######  \n\n")
+    
+    elif (c == "M"):
+        print("  #    #  \n  ##  ##  \n  # ## #  \n  #    #  \n  #    #  \n\n")
+    
+    elif (c == "N"):
+        print("  #    #  \n  ##   #  \n  # #  #  \n  #  # #  \n  #   ##  \n\n")#Glostrixin Hydrotermius
+    
+    elif (c == "O"):
+        print("  ######  \n  #    #  \n  #    #  \n  #    #  \n  ######  \n\n")
+    
+    elif (c == "P"):#Glostrixin Hydrotermius
+        print("  ######  \n  #    #  \n  ######  \n  #       \n  #       \n\n")
+    
+    elif (c == "Q"):
+        print("  ######  \n  #    #  \n  # #  #  \n  #  # #  \n  ######  \n\n")
+    
+    elif (c == "R"):
+        print("  ######  \n  #    #  \n  # ##   \n  #   #   \n  #    #  \n\n")
+    
+    elif (c == "S"):
+        print("  ######  \n  #       \n  ######  \n       #  \n  ######  \n\n")
+    #Glostrixin Hydrotermius
+    elif (c == "T"):
+        print("  ######  \n    ##    \n    ##    \n    ##    \n    ##    \n\n")
+    
+    elif (c == "U"):
+        print("  #    #  \n  #    #  \n  #    #  \n  #    #  \n  ######  \n\n")#Glostrixin Hydrotermius
+    
+    elif (c == "V"):
+        print("  #    #  \n  #    #  \n  #    #  \n   #  #   \n    ##    \n\n")
+    
+    elif (c == "W"):
+        print("  #    #  \n  #    #  \n  # ## #  \n  ##  ##  \n  #    #  \n\n")
+    #Glostrixin Hydrotermius
+    elif (c == "X"):
+        print("  #    #  \n   #  #   \n    ##    \n   #  #   \n  #    #  \n\n")
+    
+    elif (c == "Y"):
+    
+        print("  #    #  \n   #  #   \n    ##    \n    ##    \n    ##    \n\n")#Glostrixin Hydrotermius
+    
+    elif (c == "Z"):
+        print("  ######  \n      #   \n     #    \n    #     \n  ######  \n\n")
+    #Glostrixin Hydrotermius
+    elif (c == " "):
+        print("          \n          \n          \n          \n\n")
+    
+    elif (c == "."):
+        print("----  ----\n\n")#Glostrixin Hydrotermius
+
+print("______________________________________")
+print("If you like it!!! Please hit one like ")
+print("______________________________________")
